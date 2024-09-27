c270d730f01376733875c3c9d3cd800370e600c2
Fabricio Abreu <fabricio.correa.abreu@gmail.com>
2024-09-26 21:26:32 -0300

    ProfessorController
    Criação do Controller Professor para manipulação da Base de dados com HTTP GET

---
+++ EscolaPrimariaAPI/EscolaPrimariaAPI/Controllers/ProfessorController.cs
@@ -0,0 +1,40 @@
+﻿using EscolaPrimariaAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EscolaPrimariaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfessorController : ControllerBase
+    {
+
+        private readonly EscolaPrimariaContext _context;
+
+        public ProfessorController(EscolaPrimariaContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Professor>>> GetProfessor()
+        {
+
+            List<Professor> professores = await _context.Professors.ToListAsync();
+            return Ok(professores);
+
+        }
+
+        [HttpGet("{IdProfessor}")]
+        public async Task<ActionResult<Professor>> GetProfessor(int IdProfessor)
+        {
+
+            Professor professor = await _context.Professors.FindAsync(IdProfessor);
+            return Ok(professor);
+
+        }
+
+    }
+}
